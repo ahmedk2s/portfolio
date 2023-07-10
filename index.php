f5a89e4c37ca81d66dbe29ee2de7359f0a465247
ahmed58000 <ahmed_k@laposte.net>
2023-07-10 16:37:23 +0200

    login

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Accueil</li>
+            <li>Profil</li>
+            <li>Compétences</li>
+            <li>Projets</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
