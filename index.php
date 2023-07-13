727cf658ced0180c5558b0cce8e0ebdc912aa671
ahmed58000 <ahmed_k@laposte.net>
2023-07-13 16:41:07 +0200

    section

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>
+
     <nav>
         <ul class="navbar">
             <li>Accueil</li>
@@ -18,25 +19,70 @@
             <li>Contact</li>
         </ul>
     </nav>
+
     <section class="accueil">
         <div class="accueil-left-Container">
             <h1></h1>
         </div>
         <div class="accueil-right-container">
-            <img src="./imgs/accueil.jpg" alt="">
+            <img src="./imgs/ordinateur.jpg" alt="ordinateur">
         </div>
     </section>
+
     <section class="profil">
-        <div class="profil-img-container">
-            <img src="./imgs/profil.jpg" alt="">
+        <div class="profil-container">
+            <div class="profil-left-container">
+                <img src="./imgs/question.jpg" alt="point d'interrogation">
+            </div>
+            <div class="profil-right-container">
+                <h2>Qui suis-je ?</h2>
+                <p id="autotext"></p>
+                <p id="text">Hello, Je suis Ahmed, Développeur Web , motivé par les nouvelles technologies et leurs facultés à améliorer la productivité des entreprises.</p>
+            </div>
         </div>
     </section>
+
     <section class="competences">
-        <div class="competences-img-container">
-            <img src="./imgs/competences.jpg" alt="">
+        <div class="competences-container">
+            <div class="competences-left-container">
+                <h2>Mes compétences</h2>
+            </div>
+            <div class="competences-right-container">
+                <img src="./imgs/competences.jpg" alt="cravate">
+            </div>
         </div>
     </section>
-    
+
+    <section class="projets">
+        <div class="projets-container">
+            <div class="projets-container">
+                <div class="projets-left-container">
+                    <img src="./imgs/ampoule.jpg" alt="cravate">
+                </div>
+                <div class="projets-left-container">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact">
+        <div class="contact-container">
+            <div class="contact-container">
+                <div class="contact-left-container">
+                    
+                </div>
+                <div class="contact-right-container">
+                <img src="./imgs/boite-au-lettres.jpg" alt="boite au lettres">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
     <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
     <script src="script.js"></script>
 </body>
--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const textAnim = document.querySelector('h1');
-
 new Typewriter(textAnim, {
     deleteSpeed: 20
 })
 .changeDelay(20)
-.typeString('Hello,<br> I\'am Ahmed')
+.typeString('Hi,<br> I\'am Ahmed')
 .pauseFor(300)
-.typeString('<span style="color: #E55E34" font-size="4rem"><br> Developpeur web </span>')
+.typeString('<span style="color: #E55E34"><br> Developpeur web </span>')
 .start();
 
-var typed = new Typed('#element', {
-    strings: ['<span class="custom-text">Développeur web !</span>'],
-    typeSpeed: 50
-  });
 
    
